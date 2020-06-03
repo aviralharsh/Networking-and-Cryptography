1e0bd108bea7af7feabcf02d288ae39aaa0abc08
Aviral Harsh <64276191+aviralharsh@users.noreply.github.com>
2020-06-03 23:48:50 +0530

    Update net_scanner.py

--- Network Scanner/net_scanner.py
+++ Network Scanner/net_scanner.py
@@ -1,22 +1,31 @@
 #! usr/bin/env python
-
 import scapy.all as scapy
+import optparse
+import os
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="ip", help="Enter Ip range to search for")
+    (options, arguments) = parser.parse_args()
+    return options.ip
 
 def scan(ip):
-    # do scapy.ls(scapy.ARP())/scapy.ls(scapy.Ether()) to see all fields that can be set for ARP/Ether class
-    arp_request=scapy.ARP(pdst=ip)
-    broadcast=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    
-    # do print(broadcast.summary())/print(arp_request.summary()) to see output/summary
-    arp_request_broadcast=broadcast/arp_request #to append/combine Ether and ARP
-    
-    #do arp_request_broadcast.show() for more detailed info
-    
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    client_list = []
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    
-    print("IP\t\t\tMAC Address\n------------------------------------------------")
-    
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)      # to print ip source i.e client ip and to print MAC source i.e client MAC
-        
-scan("10.0.2.1/24")      # provide the range of subnet that you are working on or the subnet you want to scan.
+        #print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+    return(client_list)
+
+def print_func(returned_list):
+    print("IP\t\t\tMAC Address\t\t\tOS\n-------------------------------------------------------------------")
+    for client in returned_list:
+        print(client["ip"] + "\t\t" + client["mac"] + "\t\t" + os.name)
+
+
+ip = get_arguments()
+scan_result = scan(ip)
+print_func(scan_result)
